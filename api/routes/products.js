e629c6e1cde16ae093e162c47a3090865a6ef027
kamboj652 <kamboj652@gmail.com>
2018-12-15 15:54:44 +0100

    Handling Errors & Improving the Project Setup
        - Install module for error hanlding
        - Change code for error handling

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+
+//Handle incoming GET requests to /products
 router.get('/',(req,res,next)=>{
   res.status(200).json({
     message: 'Handling GET requests to /products'
--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
+
+//Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+//any error that occurs with the request
+app.use((req,res,next)=>{
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+//handle every kind of handle like DB, etc...
+app.use((error,req,res,next) => {
+  res.status(error.status || 500);
+  res.json({
+      error:{
+        message:error.message
+      }
+  });
+});
 
 module.exports = app;
