0f127ff15591d7cb4d7ae160d85de818f050face
kamboj652 <kamboj652@gmail.com>
2018-12-15 17:24:41 +0100

    MongoDB and Mongoose
        - Install MongoDB and Mongoose drivers and access
        - Create Model for Product

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://node-shop:'
++process.env.MONGO_ATLAS_PWD+
+'@node-rest-shop-cfql8.mongodb.net/test?retryWrites=true',{
+  useNewUrlParser: true
+});
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
